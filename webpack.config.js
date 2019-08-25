d7a75f54795a737ce8276c82df918a4215c7c92f
ben <benjamin.eisner.20@dartmouth.edu>
2019-08-25 12:40:37 -0700

    add sass and image loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,66 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const env = process.env.NODE_ENV || 'development';
+
+const finalCSSLoader = (env === 'production')
+  ? MiniCssExtractPlugin.loader
+  : { loader: 'style-loader' }; // injects css into DOM
 
 module.exports = {
   mode: env,
   entry: [
     'babel-polyfill', // fills new js features before our app
-    './src'
+    './src',
   ],
   devtool: 'source-map', // enables debugging with source in devtools
+  devServer: {
+    hot: true, // enables hot reloading
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: [
-        { loader: 'babel-loader'},
-        { loader: 'eslint-loader' },
-      ]
+          { loader: 'babel-loader' },
+          { loader: 'eslint-loader' },
+        ],
+      },
+      {
+        test: /\.s?css/,
+        use: [
+          finalCSSLoader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              useRelativePath: true,
+              name: '[name].[ext]',
+            },
+          },
+        ],
       },
     ],
   },
   plugins: [
+    new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: './index.html',
