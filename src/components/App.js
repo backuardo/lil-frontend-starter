745d703c4f6d98bba81a5572eafaacd5e4dbe75e
ben <benjamin.eisner.20@dartmouth.edu>
2019-08-25 13:01:19 -0700

    add react

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div id="app">
+      <h1>ben&apos;s frontend starter kit</h1>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
 import './style.scss';
 
-console.log('starting up');
+ReactDOM.render(<App />, document.getElementById('main'));
