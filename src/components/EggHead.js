6a6eac175e396176cd51ca80f7b537badc5a00d4
ben <benjamin.eisner.20@dartmouth.edu>
2019-09-02 13:27:41 -0700

    add link

--- src/components/EggHead.js
+++ src/components/EggHead.js
@@ -1,22 +1,25 @@
 import React from 'react';
+import packageDotJson from '../../package.json';
 
 function EggHead() {
   return (
-    <div className="egg">
-      <div className="egg-eyes">
-        <div className="egg-eye">
-          <div className="egg-eye-iris">
-            <div className="egg-eye-iris-sparkle" />
+    <a href={packageDotJson.repository}>
+      <div className="egg">
+        <div className="egg-eyes">
+          <div className="egg-eye">
+            <div className="egg-eye-iris">
+              <div className="egg-eye-iris-sparkle" />
+            </div>
+          </div>
+          <div className="egg-eye">
+            <div className="egg-eye-iris">
+              <div className="egg-eye-iris-sparkle" />
+            </div>
           </div>
         </div>
-        <div className="egg-eye">
-          <div className="egg-eye-iris">
-            <div className="egg-eye-iris-sparkle" />
-          </div>
-        </div>
+        <div className="egg-mouth" />
       </div>
-      <div className="egg-mouth" />
-    </div>
+    </a>
   );
 }
 
