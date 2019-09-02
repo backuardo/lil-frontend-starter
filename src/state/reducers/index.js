ed5a3dc38801a1d3064476f5843c234e27ca2600
ben <benjamin.eisner.20@dartmouth.edu>
2019-09-02 11:50:26 -0700

    remove state folder

--- src/state/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import CountReducer from './countReducer';
-
-const rootReducer = combineReducers({
-  count: CountReducer,
-});
-
-export default rootReducer;
